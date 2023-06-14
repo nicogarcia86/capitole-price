45c17268c47b80a1cde4f98a9474f559ae249c65
nicolas garcia <nicolasgarcia86@gmail.com>
2023-06-14 17:00:13 -0300

    H2 Database and flyway implemented

---
+++ src/main/resources/db/migration/V1__create_table_prices.sql
@@ -0,0 +1,17 @@
+--create table BRAND
+CREATE TABLE brand (
+    id INT PRIMARY KEY,
+    name VARCHAR(60)
+);
+--create table PRICES
+CREATE TABLE PRICES (
+    brand_id INTEGER,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    price_list INTEGER,
+    product_id INTEGER,
+    priority INTEGER,
+    price DOUBLE,
+    currency VARCHAR(3),
+    FOREIGN KEY (brand_id) REFERENCES BRAND(id)
+);
